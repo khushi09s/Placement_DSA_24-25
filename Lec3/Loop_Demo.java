d044ad36393a3add44c3ff1964534d23f90b95d0
khushi09s <145149546+khushi09s@users.noreply.github.com>
2024-09-21 08:29:52 +0000

    done

---
+++ Lec3/Loop_Demo.java
@@ -0,0 +1,12 @@
+package Lec3;
+
+public class Loop_Demo {
+    public static void main(String args[]) {
+        int n = 5;
+        int i =1;
+        while (i<=n) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ Lec3/Pattern0.java
@@ -0,0 +1,12 @@
+package Lec3;
+
+public class Pattern0 {
+    public static void main(String[] args) {
+        int n = 5;
+        int i  = 1;
+        while (i<=n) {
+            System.out.print("* ");
+            i++;
+        }
+    }
+}
